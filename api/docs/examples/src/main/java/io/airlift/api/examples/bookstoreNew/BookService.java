c92d071e2af774df666b29bdbd0d0af57d1b8448
Dejan Mircevski <dejan.mircevski@starburstdata.com>
2026-01-21 17:29:56 +0100

    Bookstore rebuild: add BookService

---
+++ api/docs/examples/src/main/java/io/airlift/api/examples/bookstoreNew/BookService.java
@@ -0,0 +1,8 @@
+package io.airlift.api.examples.bookstoreNew;
+
+import io.airlift.api.ApiService;
+
+@ApiService(name = "bookService", type = BookServiceType.class, description = "Manage books in the bookstore")
+public class BookService
+{
+}
---
+++ api/docs/examples/src/main/java/io/airlift/api/examples/bookstoreNew/BookServiceType.java
@@ -0,0 +1,32 @@
+package io.airlift.api.examples.bookstoreNew;
+
+import io.airlift.api.ApiServiceType;
+
+public class BookServiceType implements ApiServiceType
+{
+    // All endpoints in this service type will have URIs beginning with `<service-type-id>/api/v<service-version-number>/`.
+
+    @Override
+    public String id()
+    {
+        return "bookServiceTypeId";
+    }
+
+    @Override
+    public int version()
+    {
+        return 21;
+    }
+
+    @Override
+    public String title()
+    {
+        return "Book Service Type";
+    }
+
+    @Override
+    public String description()
+    {
+        return "BookServiceType description";
+    }
+}
